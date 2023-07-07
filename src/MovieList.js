4c1bca54117346bb45dedac54879ae69a6138498
EzienKay <kleeqers@gmail.com>
2023-07-07 18:21:52 +0100

    first commit

---
+++ src/MovieList.js
@@ -0,0 +1,54 @@
+
+
+const MovieList = [
+    {
+        id: 1,
+        img: 'https://static.netnaija.com/i/Wz27WpgBa5R.webp',
+        title: 'Hijack', 
+        description: "Let them think they're in control. When Flight KA29 is hijacked during its seven-hour journey from Dubai to London, Sam Nelson-an accomplished corporate negotiator-tries using his professional skills to save everyone on board. Will this high-risk strategy be his undoing?", 
+        posterURL: 'https://www.youtube.com/embed/WxwKzsklvJo', 
+        rating: '2',  
+        genre: "Drama, Thriller",
+        stars: "Idris Elba, Max Beesley, Archie Panjabi"  
+    },
+    {
+        id: 2,
+        img: 'https://static.netnaija.com/i/qvg70e50aze.webp',
+        title: 'HeartBeat', 
+        description: 'Ah.. I told you not to open it until 100 years. Half-human and half-vampire Seon Woo-hyeol, who was unable to become human due to a one day difference amidst 100 years, starts to live together with Joo In-hae, a woman who has no humane side, and find true warmth.', 
+        posterURL: 'https://www.youtube.com/embed/nUCSxkd6Xp8', 
+        rating: '1',
+        genre: "Comedy, Drama",
+        stars: "Ok Taec-yeon, Won Ji-an, Yoon So-hee, Park Kang-hyun"
+    
+    },
+    {
+        id: 3,
+        img: 'https://static.netnaija.com/i/m9z7oGB572r.webp',
+        title: 'Homewrecker (2023)', 
+        description: "Watch your back or she'll take your man. A married couple decides to rent out the extra room in their house to an attractive young woman who moves in armed with a set of filthy schemes.", 
+        posterURL: 'https://www.youtube.com/embed/4x52dN9aDHY', 
+        rating: '2',
+        genre: "Horror, Thriller",        
+        stars: "Shavon Majoi, Jaies Baptiste, Christopher St. Mary, Keyatta Crews"
+    
+    },
+    {
+        id: 4,
+        img: 'https://static.netnaija.com/i/no4NX2LL79M.webp',
+        title: 'Good Night', 
+        description: 'Mohan, an IT youngster with a snoring issue starts his life with Anu, the girl of his dreams, hiding his issue. However, trouble begins, when his snoring starts affecting his life and the health of his partner', 
+        posterURL: 'https://www.youtube.com/embed/yyMH_Gq2nHE', 
+        rating: '1',
+        genre: "Comedy, Drama",
+        stars: "K. Manikandan, Meetha Raghunath, Ramesh Thilak"
+    
+    }
+]
+
+export default MovieList
+
+
+
+
+
